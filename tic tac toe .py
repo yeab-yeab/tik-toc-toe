50cc7ce48c548a217e97f543f2ec416641725299
yeabsera dessalegn <71721807+yeab-yeab@users.noreply.github.com>
2020-10-01 19:05:11 +0300

    Update tic tac toe .py
    finished it

--- tic tac toe .py
+++ tic tac toe .py
@@ -12,7 +12,7 @@ game_still_going = True
 winner = None
 
 # whose turn is it
-current_player = 'x'
+current_player = 'X'
 
 # display board
 def display_board():
@@ -39,18 +39,33 @@ def play_game():
         flip_player()
 
     # the game has ended
-    if winner == 'x' or winner == 'o':
-        print(winner + 'won.')
+    if winner == 'X' or winner == 'O':
+        print(winner + ' won.')
     elif winner == None:
         print('Tie.')
 
 # handle a single turn of arbitrary player
 def handle_turn(player):
-    position = input('choose a position from 1-9: ')
-    # first of all beacuse positon is a string we have to change it into integer
-    position = int(position) - 1
 
-    board[position] = 'x'
+    print(player + '\'s turn.')
+    position = input('choose a position from 1-9: ')
+
+    valid = False
+    while not valid:
+
+        while position not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            position = input('invalid input. Choose 1-9: ')
+
+        # first of all beacuse positon is a string we have to change it into integer
+        position = int(position) - 1
+
+        if board[position] == '-':
+            valid = True
+        else:
+            print('you can\'t go there, Go again.')
+
+
+    board[position] = player
     display_board()
 
 
@@ -73,34 +88,90 @@ def check_for_winner():
 
     # get the winner
     if row_winner:
-        winner = row_winner()
+        winner = row_winner
     elif colum_winner:
         winner = colum_winner
     elif diagonal_winner:
         winner = diagonal_winner
     else:
-        winner = none
+        winner = None
     return
 
 
 
 def check_rows():
+    # setup global variables
+    global game_still_going
+    # check if any of the rows have all the same value (and is not empty)
+    row_1 = board[0] == board[1] == board[2] != '-'
+    row_2 = board[3] == board[4] == board[5] != '-'
+    row_3 = board[6] == board[7] == board[8] != '-'
+    # if any row does have a match, flag that there is a win
+    if row_1 or row_2 or row_3:
+        game_still_going = False
+    # return a winner (x or o)
+    if row_1:
+        return board[0]
+    if row_2:
+        return board[3]
+    if row_3:
+        return board[6]
     return
 
 
 def check_colums():
+    # setup global variables
+    global game_still_going
+    # check if any of the colums have all the same value (and is not empty)
+    column_1 = board[0] == board[3] == board[6] != '-'
+    column_2 = board[1] == board[4] == board[7] != '-'
+    column_3 = board[2] == board[5] == board[8] != '-'
+    # if any column does have a match, flag that there is a win
+    if column_1 or column_2 or column_3:
+        game_still_going = False
+    # return a winner (x or o)
+    if column_1:
+        return board[0]
+    if column_2:
+        return board[1]
+    if column_3:
+        return board[2]
     return
 
 
 def check_diagonals():
+    # setup global variables
+    global game_still_going
+    # check if any of the diagonals have all the same value (and is not empty)
+    diagonal_1 = board[0] == board[4] == board[8] != '-'
+    diagonal_2 = board[2] == board[4] == board[6] != '-'
+    # if any diagonal does have a match, flag that there is a win
+    if diagonal_1 or diagonal_2:
+        game_still_going = False
+    # return a winner (x or o)
+    if diagonal_1:
+        return board[0]
+    if diagonal_2:
+        return board[2]
     return
 
 
 def check_if_tie():
+    global game_still_going
+    if '-' not in board:
+        game_still_going = False
     return
 
 
 def flip_player():
+    # global varible we need
+    global current_player
+    #if the current player was X, then change it to O
+    if current_player == 'X':
+        current_player = 'O'
+    # if the current player was O, then change it to X
+    elif current_player == 'O':
+        current_player = 'X'
     return
 
 
